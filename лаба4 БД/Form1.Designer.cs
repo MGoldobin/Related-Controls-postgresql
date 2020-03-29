80b4a1571f98ee23e266a04b97a20259a2c3925c
MGoldobin <47533332+MGoldobin@users.noreply.github.com>
2020-03-29 19:54:15 +0500

    Add files via upload

---
+++ лаба4 БД/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace лаба4_БД
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxState = new System.Windows.Forms.ComboBox();
+            this.comboBoxCity = new System.Windows.Forms.ComboBox();
+            this.comboBoxLocalShop = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxState
+            // 
+            this.comboBoxState.FormattingEnabled = true;
+            this.comboBoxState.Location = new System.Drawing.Point(146, 72);
+            this.comboBoxState.Name = "comboBoxState";
+            this.comboBoxState.Size = new System.Drawing.Size(267, 21);
+            this.comboBoxState.TabIndex = 0;
+            this.comboBoxState.SelectedIndexChanged += new System.EventHandler(this.comboBoxState_SelectedIndexChanged);
+            // 
+            // comboBoxCity
+            // 
+            this.comboBoxCity.FormattingEnabled = true;
+            this.comboBoxCity.Location = new System.Drawing.Point(146, 115);
+            this.comboBoxCity.Name = "comboBoxCity";
+            this.comboBoxCity.Size = new System.Drawing.Size(267, 21);
+            this.comboBoxCity.TabIndex = 1;
+            this.comboBoxCity.SelectedIndexChanged += new System.EventHandler(this.comboBoxCity_SelectedIndexChanged);
+            // 
+            // comboBoxLocalShop
+            // 
+            this.comboBoxLocalShop.FormattingEnabled = true;
+            this.comboBoxLocalShop.Location = new System.Drawing.Point(146, 161);
+            this.comboBoxLocalShop.Name = "comboBoxLocalShop";
+            this.comboBoxLocalShop.Size = new System.Drawing.Size(267, 21);
+            this.comboBoxLocalShop.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 263);
+            this.Controls.Add(this.comboBoxLocalShop);
+            this.Controls.Add(this.comboBoxCity);
+            this.Controls.Add(this.comboBoxState);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxState;
+        private System.Windows.Forms.ComboBox comboBoxCity;
+        private System.Windows.Forms.ComboBox comboBoxLocalShop;
+    }
+}
+
---
+++ лаба4 БД/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace лаба4_БД
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            fill();
+        }
+
+
+        string ConnectionString = @"Host = localhost;Username = postgres;Password = Misha310; Database = postgres";
+
+        private async void fill()
+        {
+            var Connect = new NpgsqlConnection(ConnectionString);
+            NpgsqlDataAdapter adapState = new NpgsqlDataAdapter("SELECT statevid, statename FROM state", Connect);
+            DataTable dtState = new DataTable();
+            adapState.Fill(dtState);
+            comboBoxState.DataSource = dtState;
+            comboBoxState.DisplayMember = "statename";
+            comboBoxState.ValueMember = "statename";
+        }
+
+        private void comboBoxState_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var Connect = new NpgsqlConnection(ConnectionString);
+            NpgsqlDataAdapter adapCity = new NpgsqlDataAdapter("SELECT city.cityname FROM city, state WHERE state.statename='" + comboBoxState.SelectedValue.ToString() + "' AND  city.state_id=state.statevid", Connect);
+            DataTable dtCity = new DataTable();
+            adapCity.Fill(dtCity);
+            if (dtCity.Rows.Count > 0)
+            {
+                comboBoxCity.DataSource = dtCity;
+                comboBoxCity.DisplayMember = "cityname";
+                comboBoxCity.ValueMember = "cityname";
+            }
+            else
+            {
+                comboBoxCity.DataSource = null;
+                comboBoxCity.Text = "NULL";
+            }
+        }
+
+        private void comboBoxCity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            var Connect = new NpgsqlConnection(ConnectionString);
+            NpgsqlDataAdapter adapLocalShop = new NpgsqlDataAdapter("SELECT addres FROM localshop, city WHERE city.cityname='" + comboBoxCity.SelectedValue.ToString() + "' AND  localshop.city_id=city.cityid", Connect);
+            DataTable dtLocalShop = new DataTable();
+            adapLocalShop.Fill(dtLocalShop);
+            if (dtLocalShop.Rows.Count > 0)
+            {
+                comboBoxLocalShop.DataSource = dtLocalShop;
+                comboBoxLocalShop.DisplayMember = "addres";
+                comboBoxLocalShop.ValueMember = "addres";
+            }
+            else
+            {
+                comboBoxLocalShop.DataSource = null;
+                comboBoxLocalShop.Text = "NULL";
+            }
+        }
+    }
+}
